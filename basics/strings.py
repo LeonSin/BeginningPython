58842d994d5b6c4e844f5b102d6deeb80d7cc72e
Leon.R.Sine <leonsin112358@gmail.com>
2014-09-09 10:48:56 +0800

    project init

---
+++ basics/strings.py
@@ -0,0 +1,18 @@
+
+# repr creates a string that is a representation 
+# of the value as a legal Python expression.
+print repr("hello, world!")
+print repr(10000L)
+
+# str simply converts a value into a string in some reasonable
+# fashion that will probably be understood by a user.
+
+print str("Hello, world!")
+print str(10000L)
+
+# ` is backstick which locates on the left of num 1 in keyboard
+# however backsticks are removed in Python 3.0
+temp = 42
+print "the temperature is " + `temp`
+
+
