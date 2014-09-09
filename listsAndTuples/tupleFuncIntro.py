3a0df0e7bb087ffffe59e0c932acbbe09740780b
Leon.R.Sine <leonsin112358@gmail.com>
2014-09-09 21:10:42 +0800

    add tuple example

---
+++ listsAndTuples/tupleFuncIntro.py
@@ -0,0 +1,19 @@
+# Tuple : Immutable Sequences
+print 1, 2, 3
+print (1, 2, 3)
+print type((1, 2, 3))
+print () # an empty tuple
+
+# a tuple containing a single value.
+print 42
+print (42,)
+
+#(42) is exactly the same as 42.
+print 3*(42)
+print 3*(42,)
+
+# Tuple function
+print tuple([1, 2, 3])
+print tuple('abc')
+print tuple((1, 2, 3))
+
