52776e9579c319e785d347f4166ad2438f3c5919
Leon.R.Sine <leonsin112358@gmail.com>
2014-09-09 11:23:54 +0800

    chapter 1 summary

---
+++ basics/notes.md
@@ -0,0 +1,17 @@
+Function|Description
+--------|-----------
+abs(number)|Returns the absolute value of a number
+cmath.sqrt(number)|Returns the square root; works with negative numbers
+float(object)|Converts a string or number to a floating-point number
+help()|Offers interactive help
+input(prompt)|Gets input from the user
+int(object)|Converts a string or number to an integer
+long(object)|Converts a string or number to a long integer
+math.ceil(number)|Returns the ceiling of a number as a float
+math.floor(number)|Returns the floor of a number as a float
+math.sqrt(number)|Returns the square root; doesn’t work with negative numbers
+pow(x, y[, z])|Returns x to the power of y (modulo z)
+raw_input(prompt)|Gets input from the user, as a string
+repr(object)|Returns a string representation of a value
+round(number[, ndigits])|Rounds a number to a given precision
+str(object)|Converts a value to a string
--- basics/strings.py
+++ basics/strings.py
@@ -3,16 +3,43 @@
 # of the value as a legal Python expression.
 print repr("hello, world!")
 print repr(10000L)
-
+print '-----------'
 # str simply converts a value into a string in some reasonable
 # fashion that will probably be understood by a user.
 
 print str("Hello, world!")
 print str(10000L)
+print '-----------'
 
 # ` is backstick which locates on the left of num 1 in keyboard
 # however backsticks are removed in Python 3.0
 temp = 42
 print "the temperature is " + `temp`
+print '-----------'
+
+'''
+In short, str, repr, and backticks are three ways of converting a Python value to a string. 
+The function str makes it look good, while repr (and the backticks) tries to make the resulting 
+string a legal Python expression.
+'''
+
+# long strings - use triple quotes instead of ordinary quotes
+print '''This is a very long string.
+It continues here.
+And it's not over yet.
+"Hello, world!"
+Still here.'''
+print '-----------'
+
+# Every character you put into a raw string stays the way you wrote it:
+print r'C:\nowhere'
+print r'C:\Program Files\fnord\foo\bar\baz\frozz\bozz'
+print '-----------'
 
 
+# Unicode Strings
+# Normal strings in Python are stored internally as 8-bit ASCII,
+# while Unicode strings are stored as 16-bit Unicode.
+print u'Hello, world!'
+print '-----------'
+# In Python 3.0, all strings will be Unicode strings.
