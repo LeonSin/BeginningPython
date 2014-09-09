7ca0a3a790a3b60f2642349ed2e19c31e3a28f39
Leon.R.Sine <leonsin112358@gmail.com>
2014-09-09 13:04:19 +0800

    add sequence indexing example

---
+++ listsAndTuples/sequence.py
@@ -0,0 +1,16 @@
+# Python has 6 built-in types of sequences:
+# lists and tuples(most common)
+# strings, Unicode strings, buffer objects, and 
+# xrange objects.
+
+# Main difference between lists and tuples is that you can change
+# a list,but you can't change a tuple.
+# That's why you may see built-in functions returning tuples.
+
+# Common Sequence Operations
+
+# Indexing
+greeting = 'Hello'
+print greeting[0]
+print greeting[-1]
+print 'Hello'[1] # string literal
