a431e5744ce62c8a200569bd343757088fced2e3
Leon.R.Sine <leonsin112358@gmail.com>
2014-09-09 20:54:57 +0800

    add sorting method of list

--- listsAndTuples/listFuncIntro.py
+++ listsAndTuples/listFuncIntro.py
@@ -139,3 +139,29 @@ x = [4, 6, 2, 1, 7, 9]
 y = sorted(x)
 print x
 print y
+
+# sort in reverse order
+x = [4, 6, 2, 1, 7, 9]
+y = x[:]
+y.sort()
+y.reverse()
+print x
+print y
+
+# advanced sorting
+printSeparateLines('Advanced Sorting')
+print cmp(42, 32)
+print cmp(99, 100)
+print cmp(10, 10)
+
+numbers = [5, 2, 9, 7]
+numbers.sort(cmp)
+print numbers
+
+x = ['aardvark', 'abalone', 'acme', 'add', 'aerate']
+x.sort(key = len)
+print x
+
+x = [4, 6, 2, 1, 7, 9]
+x.sort(reverse=True)
+print x
