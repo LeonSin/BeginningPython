3832a0bbea5440e1995a78fb43e7abc05d065d56
Leon.R.Sine <leonsin112358@gmail.com>
2014-09-10 17:38:01 +0800

    string function list

---
+++ strings/stringFuncIntro.py
@@ -0,0 +1,15 @@
+# All the standard sequence operations(indexing, slicing,
+# multiplication, membership, length, minimum,and maximum)
+# work with strings, as you saw in the previous chapter.
+# Remember, however ,that strings are immutable, so all
+# kinds of item or slice assignments are illegal.
+
+def printSeparateLines(title):
+	print '-' * 5 + title + '-' * 5
+
+
+# string formatting 
+printSeparateLines('string formatting')
+format = "hello, %s. %s enough for ya?"
+values = ('world', 'Hot')
+print format % values
