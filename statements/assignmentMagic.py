fe8e79f30d584525061f31c821bfed7d435f8426
Leon.R.Sine <leonsin112358@gmail.com>
2014-09-16 22:13:24 +0800

    chained assignments

---
+++ statements/assignmentMagic.py
@@ -0,0 +1,28 @@
+# Sequence Unpacking
+x, y ,z = 1, 2, 3
+print x, y ,z
+
+# switch the contents of two (or more) 
+x, y = y, x
+print x, y ,z
+
+values = 1, 2, 3
+print values
+x, y, z = values
+print x
+
+# chained assignments
+x = y = dict( name = 'leon')
+print x
+print y 
+# 3-parameters expression( a if b else c)
+print 'x = y' if x == y else 'x <> y'
+
+x = dict( name = 'Leon R.Sine' )
+y = dict( name = 'Leon R.Sine' )
+print 'x = y' if x == y else 'x <> y'
+
+x = tuple([1, 2, 3])
+y = tuple([1, 2, 3])
+print 'x = y' if x == y else 'x <> y'
+
