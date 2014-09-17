96a33fbafd9f87706d6b00108edca97562c86f32
Leon.R.Sine <leonsin112358@gmail.com>
2014-09-17 21:53:33 +0800

    add loops

---
+++ statements/loops.py
@@ -0,0 +1,16 @@
+# while loops
+x = 1
+while x <= 100:
+	print x
+	x += 1
+
+# for loops
+words = ['this', 'is', 'an', 'ex', 'parrot']
+for word in words:
+	print word
+
+print range(0, 10)
+print range(10)
+
+for number in range(1, 101):
+	print number
