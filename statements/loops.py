42c3f6b96509c208862b58d0b63f5b82b48587f1
Leon.R.Sine <leonsin112358@gmail.com>
2014-09-20 22:59:37 +0800

    iterating over dictionaries

--- statements/loops.py
+++ statements/loops.py
@@ -14,3 +14,12 @@ print range(10)
 
 for number in range(1, 101):
 	print number
+
+# iterating over dictionaries
+d = {'x': 1, 'y': 2, 'z': 3}
+for key in d:
+	print key, 'corresponds to', d[key]
+
+print d.items()
+for key, value in d.items():
+	print key, 'corresponds to', value
