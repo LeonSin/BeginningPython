4fe7ae725691ae3b66dc8f712979461538d650d6
Leon.R.Sine <leonsin112358@gmail.com>
2014-09-22 23:13:32 +0800

    for-else clause

--- statements/breakLoops.py
+++ statements/breakLoops.py
@@ -12,4 +12,21 @@ print range(0, 10, 2)
 # continue - terminates the current loop
 # and jumps to the next one
 
+# while True/break idiom
+while True:
+	word = raw_input('Please enter a word: ')
+	if not word : break
+	# do sth. with the word
+	print 'The word was ' + word
 
+
+# for - else : else clause only executed if you
+# didn't call break
+from math import sqrt
+for n in range(99, 81, -1):
+	root = sqrt(n)
+	if root == int(root):
+		print n
+		break
+else:
+	print "Didn't find it"
