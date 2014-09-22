e3f5b39860f75c678275481511585f9be28d03cf
Leon.R.Sine <leonsin112358@gmail.com>
2014-09-22 23:36:36 +0800

    del statements

--- statements/moreStatements.py
+++ statements/moreStatements.py
@@ -1,8 +1,30 @@
 # nothing happened
+name = 'Enid'
 if name == 'Ralph Auldus Melish':
 	print 'Welcome!'
 elif name == 'Enid':
 	# Not finished yet...
+	print 'nothing happened'
 	pass
 elif name == 'Bill Gates':
 	print 'Access Denied'
+
+# deleting with del statements
+scoundrel = {'age': 42, 'first name': 'Robin', 'last name': 'of Locksley'}
+robin = scoundrel
+print scoundrel
+print robin
+scoundrel = None
+print robin
+robin = None
+print robin
+
+# use del
+x = 1
+del x
+# print x - NameError: name 'x' is not defined
+
+x = ["Hello", "world"]
+y = x
+y[1]="python"
+print x
