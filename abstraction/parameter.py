6ebf80b8b8d44d9ef025cac4a7c22d1a4d5bd68c
Leon.R.Sine <leonsin112358@gmail.com>
2014-09-24 19:10:29 +0800

    doc of a function

--- abstraction/parameter.py
+++ abstraction/parameter.py
@@ -8,3 +8,15 @@ for i in range(8):
 	fibs.append(fibs[-2] + fibs[-1])
 
 print fibs
+
+# creating your own functions
+
+# documenting fcuntions
+# If you put a string at the beginning of a function,
+# it's stored as part of the function and is called
+# a docstring
+
+def square(x):
+	'Calculates the square fo the number x.'
+	return x * x 
+print square.__doc__
