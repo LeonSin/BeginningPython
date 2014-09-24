63d6b105c6a51eed9efe7a4406e673fbabaeac92
Leon.R.Sine <leonsin112358@gmail.com>
2014-09-24 19:00:07 +0800

    fibomacci numbers

--- abstraction/parameter.py
+++ abstraction/parameter.py
@@ -1,4 +1,10 @@
-# A thorough introdution to parameters and scoping
+# thorough introdution to parameters and scoping
 
 # laziness is a virtue
 
+# Fibonacci numbers
+fibs = [0, 1]
+for i in range(8):
+	fibs.append(fibs[-2] + fibs[-1])
+
+print fibs
