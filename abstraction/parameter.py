f4bae0ba0e1f3446f411d4b3a2ac8c35823c208c
Leon.R.Sine <leonsin112358@gmail.com>
2014-09-26 21:11:54 +0800

    usage of function parameters

--- abstraction/parameter.py
+++ abstraction/parameter.py
@@ -43,3 +43,29 @@ print x
 # Parameters are kept in what is called a local scope
 
 # keyword parameters and defaults
+
+#  positional parameters are above
+
+
+def hello_1(greeting, name):
+    print '%s, %s!' % (greeting, name)
+
+
+def hello_2(name, greeting):
+    print '%s, %s!' % (name, greeting)
+
+
+hello_1('Hello', 'world')
+hello_2('Hello', 'world')
+# supply the name of your parameter that called as keyword parameters.
+hello_1(greeting='Hello', name='world')
+hello_1(name='world', greeting='Hello')
+
+
+def hello_3(greeting='Hello', name='world'):
+    """ arguments that have default values"""
+    print '%s, %s!' % (greeting, name)
+
+hello_3()
+hello_3('Greetings')
+hello_3('Greetings', 'universe')
