d660238c2766dcf564974a4fc994643b88180e95
Leon.R.Sine <leonsin112358@gmail.com>
2014-09-26 20:58:52 +0800

    Format the code

--- abstraction/parameter.py
+++ abstraction/parameter.py
@@ -1,34 +1,39 @@
-# thorough introdution to parameters and scoping
+# thorough introduction to parameters and scoping
 
 # laziness is a virtue
 
 # Fibonacci numbers
 fibs = [0, 1]
 for i in range(8):
-	fibs.append(fibs[-2] + fibs[-1])
+    fibs.append(fibs[-2] + fibs[-1])
 
 print fibs
 
 # creating your own functions
 
-# documenting fcuntions
+# documenting functions
 # If you put a string at the beginning of a function,
 # it's stored as part of the function and is called
 # a docstring
 
+
 def square(x):
-	'Calculates the square fo the number x.'
-	return x * x 
+    """Calculates the square fo the number x."""
+    return x * x
+
+
 print square.__doc__
 
 # help(square)
 
 # functions that aren't really functions
 
+
 def test():
     print 'This is printed'
 
 x = test()
+
 print x
 
 # return None
