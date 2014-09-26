531f25735a686f1568847d31c2c38e7851282069
Leon <leonsin112358@gmail.com>
2014-09-26 22:04:35 +0800

    put kinds of parameters together

--- abstraction/parameter.py
+++ abstraction/parameter.py
@@ -101,3 +101,14 @@ def print_params_3(**params):
     print params
 
 print_params_3(x=1, y=2, z=3)
+
+
+def print_params_4(x, y, z=3, *pospar, **keypar):
+    """
+    put the above together
+    """
+    print x, y, z
+    print pospar
+    print keypar
+
+print_params_4(1, 2, 3, 5, 6, 7, foo=1, bar=2)
