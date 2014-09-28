b9b652958dabd01c1f1c9638045a9b81ce1009e8
Leon <leonsin112358@gmail.com>
2014-09-28 18:53:28 +0800

    A classic recursion, factorial

--- abstraction/recursion.py
+++ abstraction/recursion.py
@@ -3,3 +3,22 @@ def recursion():
     return recursion()
 
 # recursion() # RuntimeError: maximum recursion depth exceeded
+
+# two classics: Factorial and power
+
+
+def factorial(n):
+    result = n
+    for i in range(1,n):
+        result *= i
+    return result
+
+
+def factorialWithRecursion(n):
+    if n == 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+print factorial(10)
+print factorialWithRecursion(10)
